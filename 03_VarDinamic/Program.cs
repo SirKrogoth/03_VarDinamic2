35e6816a20e23407c6d60ec1874cdc52f4f54bac
joaor <joaor@DESKTOP-ORTRKJA>
2018-07-28 13:22:07 -0300

    Tipos dynamicos

--- 03_VarDinamic/Program.cs
+++ 03_VarDinamic/Program.cs
@@ -10,7 +10,27 @@ namespace _03_VarDinamic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("OI");
+            object t1 = "";
+            object t2 = 123456;
+            t2 = "";
+
+            var v1 = "";
+            var v2 = 123456;
+            //Apresenta ERRO var v3;
+
+            dynamic d1 = new Usuario {nome = "joão"};
+
+            Console.WriteLine(d1.nome);
+            //Só apresenta erro em tempo de execução Console.WriteLine(d1.senha);
+            //RunTime Exception
+
+            d1 = "";
+            Console.ReadKey();
         }
+    }
+
+    class Usuario
+    {
+        public string nome { get; set; }
     }
 }
